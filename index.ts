d9d4241d4da7deb9a9ff547881e5690be84689e3
Manuel M <mmmvalley@gmail.com>
2025-10-26 21:27:57 +0100

    fix: missing info

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 // Import the `importDeposit` function from the file where it is implemented
-export { importDeposit } from './src/import-deposit';
+export { importDeposit } from "./src/import-deposit";
 
 // Import the `importDeposit` function from the file where it is implemented
-export { exportDeposit } from './src/export-deposit';
+export { exportDeposit } from "./src/export-deposit";
+
+// Import the `getUserSummary` function from the file where it is implemented
+export { getUserSummary } from "./src/user-summary";
